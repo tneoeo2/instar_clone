9cbdf6ecd4173d01e23c441f63c7c3b3388b6a3f
tneoeo2 <tneoeo2@gmail.com>
2023-02-21 01:12:45 +0900

    /main  페이지 경로추가

--- instar_clone/instar_clone/settings.py
+++ instar_clone/instar_clone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content',
+    'user',
+    'instar_clone',
+    # 'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'instar_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ instar_clone/instar_clone/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class Sub(APIView):
+    def get(self, request): 
+        print("get으로 호출")
+        return render(request, "instar_clone/main.html")
+    
+    def post(self, request):
+        print("post로 호출")
+        return render(request, "instar_clone/main.html")
