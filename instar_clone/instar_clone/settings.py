9d4943de263758fffaa4f0a70d4c367d1018c588
tneoeo2 <tneoeo2@gmail.com>
2023-03-04 16:48:44 +0900

    media_URL 추가 외

--- instar_clone/instar_clone/settings.py
+++ instar_clone/instar_clone/settings.py
@@ -14,13 +14,18 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v2^&@yt(c2na4h=&q6w*$8l8^!=c9hgwvh@$a^@y(uiskadz8n'
+try:
+    from .settings_secret import SECRET_KEY
+except ImportError:
+    SECRET_KEY  = 'defautl_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 각 media 파일에 대한 URL Prefix
+MEDIA_URL = '/media/'       ##항상 /로 끝나도록 설정 할 것
+
+#업로드된 파일을 저장할 디렉토리 경로
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
